2067983c7caa6ec807b42e4185cda5133afc6ef2
antonweizmann <aweizman@student.42heilbronn.de>
2024-05-17 11:04:25 +0200

    parsing folder added, gonna branch out now

--- src/parsing/parse_file.c
+++ src/parsing/parse_file.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antonweizmann <antonweizmann@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/08 22:43:17 by padam             #+#    #+#             */
-/*   Updated: 2024/05/17 10:32:18 by antonweizma      ###   ########.fr       */
+/*   Created: 2024/05/17 11:02:43 by antonweizma       #+#    #+#             */
+/*   Updated: 2024/05/17 11:03:33 by antonweizma      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-#include <stdio.h>
 
-int	main(int argc, char **argv)
+int	parse_file()
 {
-	t_vec3d	vec;
 
-	vec.x = 10.0;
-	vec.y = 8.0;
-	vec.z = 2.0;
-	if (argc == 1)
-	{
-		ft_printf("Usage: %s <arg1> <arg2> <arg3> <arg4>\n", argv[0]);
-		return (1);
-	}
-	printf("Magnitude %f\n", vec3d_len(vec));
-	return (0);
 }
