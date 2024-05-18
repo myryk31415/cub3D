a18fc7843130a5f7b0e39b0221c70f597a11f650
antonweizmann <aweizman@student.42heilbronn.de>
2024-05-18 14:12:45 +0200

    seg fault fixed

--- src/main/cleanup.c
+++ src/main/cleanup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cleanup.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
+/*   By: antonweizmann <antonweizmann@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 10:39:56 by antonweizma       #+#    #+#             */
-/*   Updated: 2024/05/18 14:04:22 by padam            ###   ########.fr       */
+/*   Updated: 2024/05/18 14:12:33 by antonweizma      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,11 @@ void	error(char *msg, t_game *game, char **file, t_map *map)
 		ft_putstr_fd(msg, 2);
 	while (i < 4)
 		free_str_array((void *)game->textures[i++].grid, &game->textures->height);
-	free_str_array((void *)map->grid, &map->height);
-	free(map);
+	if (map)
+	{
+		free_str_array((void *)map->grid, &map->height);
+		free(map);
+	}
 	free(game->textures);
 	free_str_array((void *)file, NULL);
 	exit (1);
